04919da7db9105e586abf0d3c8faa10705f0473f
Wanijya <wanijyabisen@gmail.com>
2024-01-04 18:58:56 +0530

    While Loop

---
+++ loop.py
@@ -0,0 +1,10 @@
+variable_a = 1
+while variable_a <= 10:
+    print(variable_a)
+    # variable_a = variable_a + 1
+    variable_a += 1
+
+# variable_a = 1
+# while variable_a <= 10:
+#     print(variable_a)
+#     break
--- taking_user_input.py
+++
@@ -1,7 +0,0 @@
-
-# input_variable = input("Enter your name\n")
-# print("Hello", input_variable)
-
-num_1 = int(input("Please enter number\n"))
-num_2 = int(input("Please enter number\n"))
-print("The sum is:", num_1+num_2)
