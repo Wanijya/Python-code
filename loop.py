b052f40b6f851da63a0dcdbd188020b81faee577
Wanijya <wanijyabisen@gmail.com>
2024-01-04 19:25:14 +0530

    Basics of For loop

--- loop.py
+++ loop.py
@@ -1,10 +1,22 @@
-variable_a = 1
-while variable_a <= 10:
-    print(variable_a)
-    # variable_a = variable_a + 1
-    variable_a += 1
+#for loop ka use koi bhi list ko iterable krne ke liye kiya jata hai.
 
-# variable_a = 1
-# while variable_a <= 10:
-#     print(variable_a)
-#     break
+# Example (1)
+# basic_list = ['Python', 'Java', 'cpp']
+# for i in basic_list:
+#     print(i)
+
+# # Example (2)
+# for i in range(1,6):
+#     print(i)
+
+# # Example(3)
+# message = "Python is the best language!!"
+# for ch in message:
+#     if ch.isalpha():
+#         print(ch)
+# if char.isalpha(): :- Har character ko check kiya jata hai ki woh kya alphabetic character hai, matlab kya woh kisi letter ka part hai. .isalpha() method is cheez ko check karne mein madad karta hai.
+
+# # Example(4):
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(f"({i},{j})")
